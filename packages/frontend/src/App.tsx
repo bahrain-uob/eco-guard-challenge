df73d258d19f22c4336fe1904901510cc3b4cae6
Dalmajed <danaalmajed.78@gmail.com>
2024-03-10 14:41:12 +0300

    I modified it

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -33,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Edit here</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
