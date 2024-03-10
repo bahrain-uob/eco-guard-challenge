18613721fbce472bba6ff7443ba05ef902005474
Mohammed Alsaeed <s3eedofficialemail@gmail.com>
2024-03-10 14:20:31 +0300

    Editing the Index Page (Mohammed Alsaeed)

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -33,6 +33,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <h1>"EcoGuard is the best group ever" - Mohammed Alsaeed</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
