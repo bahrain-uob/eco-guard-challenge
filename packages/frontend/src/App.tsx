7ad41e2dbdb163dd9e078020cb00ceef1d47159b
unknown <muneeraisa33@gmail.com>
2024-04-14 12:40:57 +0300

    Fixed errrors

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -19,5 +19,5 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
