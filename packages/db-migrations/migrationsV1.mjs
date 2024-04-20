324827bd2f5b2e50d5c270f35a49e3b8fd521804
Dalmajed <danaalmajed.78@gmail.com>
2024-04-20 18:05:42 +0300

    Two tables added

--- packages/db-migrations/migrationsV1.mjs
+++ packages/db-migrations/migrationsV1.mjs
@@ -1,5 +1,5 @@
 import { Kysely } from "kysely";
-
+ 
 /**
  * @param db {Kysely<any>}
  */
@@ -11,16 +11,39 @@ export async function up(db) {
     .addColumn("registration_date", "date")
     .addColumn("registration_date_expiration", "date")
     .execute();
-
-
+ 
+  await db.schema
+    .createTable("violated_cars")
+    .addColumn("car_id","integer", (col)=> col.autoIncrement().primaryKey())
+    .addColumn("license_plate", "varchar(255)", (col)=>col.notNull())
+    .addColumn("car_description", "text")
+    .addColumn("location","varchar(255)")
+    .addColumn("timestamp","timestamp")
+    .addColumn("image","varchar(255)")
+    .addColumn("status","varchar(50)")
+    .execute();
+ 
+  await db.schema
+     .createTable("light_violations")
+     .addColumn("car_id", "integer", (col) => col.autoIncrement().primaryKey())
+     .addColumn("violation_type", "varchar(50)")
+     .addColumn("license_plate", "varchar(255)", (col) => col.notNull())
+     .addColumn("timestamp", "timestamp")
+     .addColumn("location", "varchar(255)")
+     .addColumn("image", "varchar(255)")
+     .addColumn("status", "varchar(50)")
+     .execute();
+   
   }
-
+ 
 /**
  * @param db {Kysely<any>}
  */
-
+ 
 export async function down(db) {
-
+ 
   await db.schema.dropTable("registered_cars").execute();
+  await db.schema.dropTable("violated_cars").execute();
+  await db.schema.dropTable("light_violations").extecute();
  
 }
--- packages/db-migrations/migrationsV2.mjs
+++ packages/db-migrations/migrationsV2.mjs
@@ -22,5 +22,70 @@ await db
    
   ])
   .execute();
-
+ 
+await db
+   .insertInto('violated_cars')
+   .values([
+    {
+        license_plate: '434277',
+        car_description: 'Audi A6,salon black',
+        location: 'Manama',
+        timestamp: '2024-04-10 14:30:00',
+        type: 'Breaking light violation',
+        image: 'evidence/image1.jpg',
+        status: 'Completed',
+    },
+    {
+      license_plate: '987654',
+      car_description: 'Navy blue Honda civic',
+      location: 'Isa town',
+      timestamp: '2024-04-11 09:45:00',
+      type: 'Smoking violation',
+      image: 'https://example.com/image2.jpg',
+      status: 'Review',
+    },
+    {
+      license_plate: '120987',
+      car_description: 'Black Toyota',
+      location: 'Sanad',
+      timestamp: '2024-04-12 16:20:00',
+      type: 'Tailgating',
+      image: 'https://example.com/image3.jpg',
+      status: 'Rejected',
+    },
+   ])
+   .execute();
+ 
+  await db
+    .insertInto('light_violations')
+    .values([
+      {
+        license_plate: '19870',
+        timestamp: '2024-04-10 14:30:00',
+        image: 'https://example.com/light_violation1.jpg',
+        status: 'Review',
+        location: 'Sitra',
+        violation_type: 'Breaklight',
+      },
+      {
+        license_plate: '87651',
+        timestamp: '2024-04-11 09:45:00',
+        image: 'https://example.com/light_violation2.jpg',
+        status: 'Reviewed',
+        location: 'Jidali',
+        violation_type: 'Headlight',
+      },
+      {
+        license_plate: '874590',
+        timestamp: '2024-04-12 16:20:00',
+        image: 'https://example.com/light_violation3.jpg',
+        status: 'Checked',
+        location: 'Manama',
+        violation_type: 'Breaklight',
+      },
+    ])
+    .execute();
+ 
+ 
 }
+ 
