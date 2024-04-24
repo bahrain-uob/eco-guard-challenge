8858e50a26261d855178322593d8a0dcd02740db
yomna khalid ahmed <yomnakhalidahmed@yomnas-MacBook-Air.local>
2024-04-24 14:40:29 +0300

    fixed dropdown error

--- packages/frontend/src/Dropdown.tsx
+++ packages/frontend/src/Dropdown.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+interface DropdownProps {
+    button: JSX.Element;
+    children: JSX.Element;
+    classNames: string;
+    animation?: string; // Optional prop
+  }
+  
+   
 
-const Dropdown = (props) => {
+const Dropdown = (props : DropdownProps) => {
   const wrapperRef = React.useRef(null);
   const [openWrapper, setOpenWrapper] = React.useState(false);
   //useOutsideAlerter(wrapperRef, setOpenWrapper);
